30a8477512f89deb088021f5d08660a29216cfef
mulan <mulan114@yahoo.com>
2019-07-14 15:28:23 +0100

    atlas password update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://mulan114:<password>@capstone2-dmplh.mongodb.net/test?retryWrites=true&w=majority';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://mulan114:mulan114@capstone2-dmplh.mongodb.net/test?retryWrites=true&w=majority';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-lodgefinance-app';
 exports.PORT = process.env.PORT || 8080;
