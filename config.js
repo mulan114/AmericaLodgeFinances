322954958fbb0677c83ac2a6cd71cd2df2295c47
mulan <mulan114@yahoo.com>
2019-08-12 16:48:12 -0400

    updated mongodb pw

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/lodgefinance-app';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://mulan114:mulan114@capstone2-dmplh.mongodb.net/test?retryWrites=true&w=majority';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://mulan114:Q1PL4V&8xh&7a!@capstone2-dmplh.mongodb.net/test?retryWrites=true&w=majority';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-lodgefinance-app';
 exports.PORT = process.env.PORT || 8080;
 
