b39fc93c8ce8fb10d46344f6aa79adff9b147135
mulan <mulan114@yahoo.com>
2019-09-01 15:28:23 -0400

    update atlas user

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/lodgefinance-app';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://mentor:mentor@capstone2-dmplh.mongodb.net/test?retryWrites=true&w=majority';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://capstone:thinkful@capstone2-dmplh.mongodb.net/test?retryWrites=true&w=majority';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-lodgefinance-app';
 exports.PORT = process.env.PORT || 8080;
 
